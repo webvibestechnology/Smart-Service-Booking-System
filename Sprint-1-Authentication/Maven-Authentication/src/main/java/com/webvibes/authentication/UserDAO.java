ff9d89326803213fa8bfecd25067c3b683d61ca2
Divya Chavan <divyachavan26004@gmail.com>
2026-07-08 13:52:09 +0530

    add user DAO logic

---
+++ Sprint-1-Authentication/Maven-Authentication/src/main/java/com/webvibes/authentication/UserDAO.java
@@ -0,0 +1,40 @@
+package com.webvibes.authentication;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+	public String registerUser(User user) {
+		String checkEmailQuery = "SELECT email FROM user WHERE email = ? ";
+		String insertQuery = "INSERT INTO user (full_name, email, mobile, password, role) VALUES (?, ?, ?, ?, 'USER')";
+		
+		try (Connection conn = DBConnection.getConnection()) {
+			
+				try(PreparedStatement psCheck = conn.prepareStatement(checkEmailQuery)){
+					psCheck.setString(1,  user .getEmail());
+					try (ResultSet rs = psCheck.executeQuery()) {
+						if(rs.next()) {
+							return "duplicate";
+						}
+					}
+				}
+				try (PreparedStatement psInsert = conn.prepareStatement(insertQuery)) {
+					psInsert.setString(1, user.getName());
+					psInsert.setString(2, user.getEmail());
+					psInsert.setString(3, user.getMobile());
+					psInsert.setString(4, user.getPassword());
+					
+					int rows = psInsert.executeUpdate();
+					if (rows > 0) {
+						return "Success";
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
+		return "failed";
+	}
+
+}
