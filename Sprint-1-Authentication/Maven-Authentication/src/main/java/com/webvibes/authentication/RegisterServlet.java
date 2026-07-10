d76bbc27c456cb9173898129bcd24ba4cfca22c4
Divya Chavan <divyachavan26004@gmail.com>
2026-07-10 13:51:42 +0530

    update

--- Sprint-1-Authentication/Maven-Authentication/src/main/java/com/webvibes/authentication/RegisterServlet.java
+++ Sprint-1-Authentication/Maven-Authentication/src/main/java/com/webvibes/authentication/RegisterServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/profile")
+@WebServlet("/pages/profile")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
