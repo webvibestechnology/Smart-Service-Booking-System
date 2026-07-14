8c61922ce74e5605b39154c247c767ef0420599d
Divya Chavan <divyachavan26004@gmail.com>
2026-07-14 16:16:36 +0530

    complete update service servlet logic

--- Sprint-2-Service-Module/Maven-Services/src/main/java/com/webvibes/UpdateServiceServlet.java
+++ Sprint-2-Service-Module/Maven-Services/src/main/java/com/webvibes/UpdateServiceServlet.java
@@ -1,5 +1,36 @@
 package com.webvibes;
 
-public class UpdateServiceServlet {
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/updateService")
+public class UpdateServiceServlet extends HttpServlet  {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        double price = Double.parseDouble(request.getParameter("price"));
+        String description = request.getParameter("description");
+
+        Service service = new Service(id, name, price, description);
+        ServiceDAO serviceDAO = new ServiceDAO();
+
+        boolean  isUpdate =  serviceDAO.updateService(service);
+        
+        if (isUpdated) {
+            response.sendRedirect("pages/dashboard.jsp?msg=updatesuccess");
+        } else {
+            response.sendRedirect("pages/updateService.jsp?error=updatefailed");
+        }
+    }
+
 
 }
+ 
