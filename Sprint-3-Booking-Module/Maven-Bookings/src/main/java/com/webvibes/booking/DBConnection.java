bd7c785bcf51295ca188629773f07a2860a0e940
Unnati-Chhajed <unnatichhajed2005@gmail.com>
2026-07-22 19:16:16 +0530

    update DBConnection.java

--- Sprint-3-Booking-Module/Maven-Bookings/src/main/java/com/webvibes/booking/DBConnection.java
+++ Sprint-3-Booking-Module/Maven-Bookings/src/main/java/com/webvibes/booking/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection 
 {
-    private static final String URL = "jdbc:mysql://localhost:3306/booking_module_db?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/booking_module_db";
     private static final String USER = "root";
     private static final String PASSWORD = "unnati@2005"; 
 
