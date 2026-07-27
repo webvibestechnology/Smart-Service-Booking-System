265d93f76cdc1ac6e307377ceb4d4fb54db33f07
Unnati-Chhajed <unnatichhajed2005@gmail.com>
2026-07-27 19:20:32 +0530

    create manageusersservlet.java

--- Sprint-4-Dashboard/Maven-dashboard/src/main/java/com/webvibes/ManageUsersServlet.java
+++ Sprint-4-Dashboard/Maven-dashboard/src/main/java/com/webvibes/ManageUsersServlet.java
@@ -1,5 +1,43 @@
 package com.webvibes;
 
-public class ManageUsersServlet {
+import java.io.IOException;
+import java.util.List;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/manageUsers")
+public class ManageUsersServlet extends HttpServlet 
+{
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        
+        try 
+        {
+            
+            DashboardDAO dashboardDAO = new DashboardDAO();
+            List<User> userList = dashboardDAO.getAllUsers(); 
+
+            
+            request.setAttribute("userList", userList);
+
+        } catch (Exception e) 
+        {
+            e.printStackTrace();
+            request.setAttribute("errorMessage", "Error loading users from database.");
+        }
+
+        
+        request.getRequestDispatcher("/pages/manageUsers.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        doGet(request, response);
+    }
 }
